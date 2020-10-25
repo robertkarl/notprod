a6948036cc48c290d01f0d2d1c643ce58c369b77
Robert Karl <robertkarl@users.noreply.github.com>
2020-10-25 15:32:48 -0700

    More details in dev log entries and qps works.

--- lvl1/docker/src/entry.sh
+++ lvl1/docker/src/entry.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env sh
 nginx&
-PYTHONPATH=lvl1 uwsgi --ini lvl1.ini 2>&1 &
+PYTHONPATH=lvl1 uwsgi --ini /etc/lvl1.ini 2>&1 1>/dev/null &
 
-echo "Suzy, welcome to your Snoog, Inc. development instance."
+echo "\n\n\nuzy, welcome to your Snoog, Inc. development instance.\n\n\n"
 bash
--- lvl1/docker/src/qps.py
+++ lvl1/docker/src/qps.py
@@ -1,14 +1,25 @@
+import subprocess
 from subprocess import Popen, PIPE
 import time
 
-COUNT = 1
+COUNT = 10
 start = time.time()
+processes = []
 for i in range(COUNT):
-	p1 = Popen(["curl", "http://localhost/process_payment"], stdout=PIPE)
-	p1.communicate()
-delta = time.time() - start
-QPS = delta / COUNT
-if QPS > 3:
-	print("URL of next level is https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-print("{} QPS is {}".format("\n" * 10, delta / COUNT))
+    p1 = Popen(["curl", "http://localhost/process_payment"], stdout=PIPE, stderr=subprocess.DEVNULL)
+    processes.append(p1)
 
+while len(processes ) > 0:
+    curr = processes[0]
+    while curr.poll() is None:
+        print('Still calculating QPS')
+        time.sleep(1)
+    processes.pop(0)
+
+delta = time.time() - start
+QPS = COUNT / delta
+print("{} QPS is {}.".format("\n" * 10, QPS))
+if QPS > 3:
+    print("URL of next level is https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+else:
+    print("we are still bleeding money.")
