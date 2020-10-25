f2be8188f915ab387005927cf48008973f3cf680
Robert Karl <robertkarl@users.noreply.github.com>
2020-10-25 15:43:49 -0700

    a

--- lvl1/docker/src/entry.sh
+++ lvl1/docker/src/entry.sh
@@ -2,5 +2,5 @@
 nginx&
 PYTHONPATH=lvl1 uwsgi --ini /etc/lvl1.ini 2>&1 1>/dev/null &
 
-echo "\n\n\nuzy, welcome to your Snoog, Inc. development instance.\n\n\n"
+echo "\n\n\nSuzy, welcome to your Snoog, Inc. development instance.\n\n\n"
 bash
