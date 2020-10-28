4970cd8415184dcee53884105cd5fff45ca9adf6
Robert Karl <robertkarl@users.noreply.github.com>
2020-10-28 08:41:46 -0700

    Move to zsh so shell prints on startup.

--- lvl1/docker/infra/entry.sh
+++ lvl1/docker/infra/entry.sh
@@ -3,4 +3,4 @@ nginx 2>/dev/null &
 PYTHONPATH=/private/lvl1 uwsgi --ini /root/lvl1.ini  &
 
 echo "\n\n\n\n\n\n\n\n\nSuzy, welcome to your Tranq, Inc. development instance.\n\n\n\n\n\n\n\n\n"
-bash
+zsh
